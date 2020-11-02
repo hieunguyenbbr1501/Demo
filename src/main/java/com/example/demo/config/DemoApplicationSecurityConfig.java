3c95962bc5dfc78059594fe5a2ae54c41771d5e1
hieu nguyen <hieunguyen.bbr1501@gmail.com>
2020-11-02 15:28:17 +0700

    update

--- src/main/java/com/example/demo/config/DemoApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/config/DemoApplicationSecurityConfig.java
@@ -1,22 +1,28 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
 @Configuration
-@EnableWebMvcSecurity
-@EnableGlobalAuthentication
 public class DemoApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//      http.authorizeRequests().antMatchers("/", "/index", "/quizStart")
-//              .permitAll().anyRequest().authenticated();
-//      http.formLogin().loginPage("/login").permitAll().and().logout()
-//              .permitAll();
-    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.inMemoryAuthentication().withUser("test").password("test")
-                .roles("USER");
+        http
+                .authorizeRequests()
+                .antMatchers("/public/**", "/resources/**","/resources/public/**").permitAll()
+                .antMatchers("/", "/home", "/link").permitAll()
+                .antMatchers("/css/**", "/js/**", "/img/**", "**/favicon.ico").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated().and()
+                .formLogin().loginPage("/login").permitAll().and()
+                .logout().permitAll();
     }
 }
