4182d0bee2afe811b2a3f396c972820053e76e6c
hieu nguyen <hieunguyen.bbr1501@gmail.com>
2020-11-16 20:47:54 +0700

    update Major view

--- src/main/java/com/example/demo/entity/Major.java
+++ src/main/java/com/example/demo/entity/Major.java
@@ -18,7 +18,7 @@ import java.util.Date;
 @EntityListeners(AuditingEntityListener.class)
 public class Major {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
