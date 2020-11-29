1128a82add2204c7794b87f38d128ab80b844b17
hieu <hieunguyen.bbr1501@gmail.com>
2020-11-29 22:16:18 +0700

    Ci/cd pipeline test

--- src/main/java/com/example/demo/controller/SemesterController.java
+++ src/main/java/com/example/demo/controller/SemesterController.java
@@ -1,44 +1,44 @@
-//package com.example.demo.controller;
-//
-//import com.example.demo.entity.Semester;
-//import com.example.demo.service.SemesterService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//@Controller
-//public class SemesterController {
-//    @Autowired
-//    private SemesterService semesterService;
-//
-//    @GetMapping(value = "/semester")
-//    public ModelAndView index(Model model) {
-//        ModelAndView mav = new ModelAndView();
-//        mav.setViewName("semester_management");
-//        model.addAttribute("semesters", semesterService.getAllSemester() );
-//        return new ModelAndView("semester_management");
-//    }
-//
-//    @PostMapping(value = "/manager/semester")
-//    @ResponseBody
-//    public Semester add(@RequestBody Semester semester) {
-//        return semesterService.addOrUpdateSemester(semester);
-//    }
-//    @DeleteMapping(value = "/manager/semester/{id}")
-//    public ResponseEntity delete(@PathVariable(value = "id")long id) {
-//        semesterService.deleteSemester(id);
-//        return ResponseEntity.ok().build();
-//    }
-//    @PutMapping(value = "/manager/semester")
-//    @ResponseBody
-//    public Semester update(@RequestBody Semester semester) {
-//        Semester old = semesterService.getSemesterById(semester.getId());
-//        old.setName(semester.getName());
-//        old.setDescription(semester.getDescription());
-//        old.setYear(semester.getYear());
-//        return semesterService.addOrUpdateSemester(old);
-//    }
-//}
+package com.example.demo.controller;
+
+import com.example.demo.entity.Semester;
+import com.example.demo.service.SemesterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SemesterController {
+    @Autowired
+    private SemesterService semesterService;
+
+    @GetMapping(value = "/semester")
+    public ModelAndView index(Model model) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("semester_management");
+        model.addAttribute("semesters", semesterService.getAllSemester() );
+        return new ModelAndView("semester_management");
+    }
+
+    @PostMapping(value = "/manager/semester")
+    @ResponseBody
+    public Semester add(@RequestBody Semester semester) {
+        return semesterService.addOrUpdateSemester(semester);
+    }
+    @DeleteMapping(value = "/manager/semester/{id}")
+    public ResponseEntity delete(@PathVariable(value = "id")long id) {
+        semesterService.deleteSemester(id);
+        return ResponseEntity.ok().build();
+    }
+    @PutMapping(value = "/manager/semester")
+    @ResponseBody
+    public Semester update(@RequestBody Semester semester) {
+        Semester old = semesterService.getSemesterById(semester.getId());
+        old.setName(semester.getName());
+        old.setDescription(semester.getDescription());
+        old.setYear(semester.getYear());
+        return semesterService.addOrUpdateSemester(old);
+    }
+}
